f07fcf102f4f566dc1fa16f6836515394471f1d7
FlavioHenriquee <114579988+FlavioHenriquee@users.noreply.github.com>
2022-11-15 12:11:20 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('#example').DataTable({
-   
-    "language": {
+        responsive: true,
+        "language": {
         "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"
-    }
+        }
     });
 });
