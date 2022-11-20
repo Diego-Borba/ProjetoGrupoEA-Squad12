8136927f48a592b95a02fe26ee1f87655e7d1772
Diego Borba <diegoborbas03@gmail.com>
2022-11-20 20:05:17 -0300

    Adicionando responsividade ao datatables

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
     $('#tabela_chamados').DataTable({
-        responsive: true,
+        
         "language": {
         "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"
         }
     });
+    responsive : true;
 });
