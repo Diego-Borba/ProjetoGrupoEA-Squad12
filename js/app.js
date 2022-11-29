a7356d716d1edf52334303ade1f63bce03cd5a7d
Diego Borba <diegoborbas03@gmail.com>
2022-11-29 01:15:00 -0300

    elementos css na tela cadastro

--- js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#tabela_chamados').DataTable({
-        
-        "language": {
-        "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"
-        }
-    });
-    responsive : true;
-});
